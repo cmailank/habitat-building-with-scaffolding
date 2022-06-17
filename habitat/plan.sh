b89c28dab43a436acf176042b3321beba838bcd5
cmailank <cmailank@github.com>
2022-06-17 14:27:27 +0530

    updates and fixes

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
-pkg_name=meme-machine1
-pkg_origin=cm_tryhabitat
+pkg_name=meme-machine
+pkg_origin=c_tryhabitat
 pkg_version="0.1.0"
 pkg_scaffolding="core/scaffolding-ruby"
 pkg_deps=( core/imagemagick )
